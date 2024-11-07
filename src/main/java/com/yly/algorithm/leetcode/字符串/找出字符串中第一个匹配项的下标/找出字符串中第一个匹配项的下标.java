ca6f65bb297960bca1217aa1ab799a5768a1f5cd
yly <2337352929@qq.com>
2024-11-08 00:36:39 +0800

    链表

---
+++ src/main/java/com/yly/algorithm/leetcode/字符串/找出字符串中第一个匹配项的下标/找出字符串中第一个匹配项的下标.java
@@ -0,0 +1,20 @@
+package com.yly.algorithm.leetcode.字符串.找出字符串中第一个匹配项的下标;
+
+public class 找出字符串中第一个匹配项的下标 {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/yly/algorithm/链表/环形链表ii/环形链表ii.java
@@ -0,0 +1,54 @@
+package com.yly.algorithm.链表.环形链表ii;
+
+import com.yly.algorithm.链表.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 环形链表ii {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+
+        ListNode node = head;
+        while (node != null) {
+            if (visited.contains(node)) {
+                return node;
+            }
+            visited.add(node);
+            node = node.next;
+        }
+        return null;
+    }
+
+    /**
+     * 快慢指针
+     */
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
+
+
+
+
+ 
