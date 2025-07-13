8f0c94e85037cf51b7063e72ec24912ae25bd50b
yly <2337352929@qq.com>
2025-07-13 17:40:49 +0800

    接雨水 双指针

--- src/main/java/com/yly/algorithm/双指针/相向双指针/接雨水/接雨水.java
+++ src/main/java/com/yly/algorithm/双指针/相向双指针/接雨水/接雨水.java
@@ -3,25 +3,26 @@ package com.yly.algorithm.双指针.相向双指针.接雨水;
 
 public class 接雨水 {
     int trap(int[] height) {
-        if (height.length == 0) return 0;
-        int n = height.length;
+        if (height.length == 0){
+            return 0;
+        }
         int left = 0;
-        int right = n - 1;
+        int right = height.length - 1;
         int ans = 0;
 
-        int l_max = height[0];
-        int r_max = height[n - 1];
+        int leftMax = height[0];
+        int rightMax = height[right];
 
         while (left <= right) {
-            l_max = Math.max(l_max, height[left]);
-            r_max = Math.max(r_max, height[right]);
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
 
             // ans += min(l_max, r_max) - height[i]
-            if (l_max < r_max) {
-                ans += l_max - height[left];
+            if (leftMax < rightMax) {
+                ans += leftMax - height[left];
                 left++;
             } else {
-                ans += r_max - height[right];
+                ans += rightMax - height[right];
                 right--;
             }
         }
